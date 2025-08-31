c88e4360c846025e1204c50fcaa9eb6c94e7c595
sergiomartintejedor <itsergiomartin@gmail.com>
2025-08-31 02:54:15 +0200

    Limpieza del java desde el que se carga y creación del primer jar

--- src/main/java/com/pruebadevalor/quedadas/escapes/prubadevalor_escapes/PrubadevalorEscapesApplication.java
+++ src/main/java/com/pruebadevalor/quedadas/escapes/prubadevalor_escapes/PrubadevalorEscapesApplication.java
@@ -1,30 +1,13 @@
 package com.pruebadevalor.quedadas.escapes.prubadevalor_escapes;
 
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.pruebadevalor.quedadas.escapes.prubadevalor_escapes.entities.Person;
-import com.pruebadevalor.quedadas.escapes.prubadevalor_escapes.repositories.PersonRepository;
 
 @SpringBootApplication
 public class PrubadevalorEscapesApplication implements CommandLineRunner {
 
-	@Autowired
-	private PersonRepository personRepository;
 
-	/*@Autowired
-	private PersonGroupRepository personGroupRepository;
-
-	@Autowired
-	private EscapeGroupRepository groupRepository;
-
-    @Autowired
-    private UserRepository userRepository;*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrubadevalorEscapesApplication.class, args);
